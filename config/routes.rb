4fc5249efbe8ea84c6742fce4e85986b4d2503bf
Michael Lewandowski <milewgit@gmail.com>
2014-12-23 23:11:04 -0500

    Strip out everything but the index action on contents_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :contents, except: [:new, :edit]
+  root 'contents#index'
+  resources :contents, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
